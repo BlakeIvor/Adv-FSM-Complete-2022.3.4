2d6849f74e27518634145a0635decdedc65bfbf9
Blake S <blakeivorshea@gmail.com>
2024-10-06 14:00:58 -0400

    Create OffDutyState empty

---
+++ Assets/Scripts/AdvancedFSM/OffDutyState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class OffDutyState : FSMState
+{
+
+    private float timer;
+    
+
+    public OffDutyState(Transform[] wp)
+    {
+        
+
+    }
+
+    public override void Reason(Transform player, Transform npc)
+    {
+
+        
+
+    }
+
+    public override void Act(Transform player, Transform npc)
+    {
+
+
+
+
+    }
+}
